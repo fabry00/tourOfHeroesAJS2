42d3a2f435e636d1842ddcf928c1edfaa7e42ff1
fabry <fabry_00@hotmail.com>
2016-07-12 20:43:47 +0200

    added http and styles

--- app/hero-detail.component.js
+++ app/hero-detail.component.js
@@ -11,10 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
 var hero_service_1 = require('./hero.service');
+var hero_1 = require('./hero');
 var HeroDetailComponent = (function () {
     function HeroDetailComponent(heroService, route) {
         this.heroService = heroService;
         this.route = route;
+        this.close = new core_1.EventEmitter();
+        this.navigated = false; // true if navigated here
     }
     HeroDetailComponent.prototype.ngOnInit = function () {
         var _this = this;
@@ -22,24 +25,56 @@ var HeroDetailComponent = (function () {
         // observable to extract the id parameter value from the ActivateRoute
         //  service and use the HeroService to fetch the hero with that id.
         this.sub = this.route.params.subscribe(function (params) {
-            // The hero id is a number. Route parameters are always 
-            // strings. So we convert the route parameter value to a
-            // number with the JavaScript (+) operator.
-            var id = +params['id'];
-            _this.heroService.getHero(id)
-                .then(function (hero) { return _this.hero = hero; });
+            if (params['id'] !== undefined) {
+                // The hero id is a number. Route parameters are always 
+                // strings. So we convert the route parameter value to a
+                // number with the JavaScript (+) operator.
+                var id = +params['id'];
+                _this.navigated = true;
+                _this.heroService.getHero(id)
+                    .then(function (hero) { return _this.hero = hero; });
+            }
+            else {
+                _this.navigated = false;
+                _this.hero = new hero_1.Hero();
+            }
         });
     };
     HeroDetailComponent.prototype.ngOnDestroy = function () {
         // Inside the ngOnDestroy lifecycle hook, we unsubscribe from the params subscription.
         this.sub.unsubscribe();
     };
-    HeroDetailComponent.prototype.goBack = function () {
+    HeroDetailComponent.prototype.save = function () {
+        var _this = this;
+        this.heroService
+            .save(this.hero)
+            .then(function (hero) {
+            _this.hero = hero; // saved hero, w/ id if new
+            _this.goBack(hero);
+        })
+            .catch(function (error) { return _this.error = error; }); // TODO: Display error message
+    };
+    HeroDetailComponent.prototype.goBack = function (savedHero) {
         // Going back too far could take us out of the application. 
         // That's acceptable in a demo. We'd guard against it in a real application, 
         // perhaps with the CanDeactivate guard. (https://angular.io/docs/ts/latest/api/router/index/CanDeactivate-interface.html)
-        window.history.back();
+        if (savedHero === void 0) { savedHero = null; }
+        // Here we call emit to notify that we just added or modified a hero. 
+        // HeroesComponent is listening for this notification and will automatically
+        // refresh the list of heroes to include our recent updates.
+        this.close.emit(savedHero);
+        if (this.navigated) {
+            window.history.back();
+        }
     };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', hero_1.Hero)
+    ], HeroDetailComponent.prototype, "hero", void 0);
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], HeroDetailComponent.prototype, "close", void 0);
     HeroDetailComponent = __decorate([
         core_1.Component({
             selector: 'my-hero-detail',
--- app/hero.service.js
+++ app/hero.service.js
@@ -9,30 +9,97 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var mock_heroes_1 = require('./mock-heroes');
+var http_1 = require('@angular/http');
+// Used before simulate HTTP
+//import { HEROES } from './mock-heroes';
+require('rxjs/add/operator/toPromise');
 var HeroService = (function () {
-    function HeroService() {
+    function HeroService(http) {
+        this.http = http;
+        this.heroesUrl = 'app/heroes'; // URL to web api
     }
+    // Used before simulate HTTP
+    /*
+    getHeroes() {
+      // A Promise is ... well it's a promise to call us back later when the results are ready
+      // We're still mocking the data. We're simulating the behavior of an ultra-fast, zero-latency server,
+      // by returning an immediately resolved Promise with our mock heroes as the result.
+      
+      //return Promise.resolve(HEROES);
+    }*/
     HeroService.prototype.getHeroes = function () {
-        // A Promise is ... well it's a promise to call us back later when the results are ready
-        // We're still mocking the data. We're simulating the behavior of an ultra-fast, zero-latency server, 
-        // by returning an immediately resolved Promise with our mock heroes as the result.
-        return Promise.resolve(mock_heroes_1.HEROES);
+        /**
+         * We're still returning a Promise but we're creating it differently.
+         * The Angular http.get returns an RxJS Observable. Observables are a powerful
+         * way to manage asynchronous data flows. We'll learn about Observables later.
+         *
+         */
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(function (response) { return response.json().data; })
+            .catch(this.handleError);
     };
     HeroService.prototype.getHeroesSlowly = function () {
-        return new Promise(function (resolve) {
-            return setTimeout(function () { return resolve(mock_heroes_1.HEROES); }, 2000);
-        } // 2 seconds
-         // 2 seconds
-        );
+        /*return new Promise<Hero[]>(resolve =>
+          setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+        );*/
     };
     HeroService.prototype.getHero = function (id) {
-        return this.getHeroesSlowly()
+        return this.getHeroes()
             .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
+    };
+    HeroService.prototype.delete = function (hero) {
+        var headers = new http_1.Headers();
+        headers.append('Content-Type', 'application/json');
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http
+            .delete(url, headers)
+            .toPromise()
+            .catch(this.handleError);
+    };
+    /**
+     * We combine the call to the private post and put methods in a single save method.
+     * This simplifies the public API and makes the integration
+     * with HeroDetailComponent easier. HeroService determines which method to call based
+     * on the state of the hero object. If the hero already has an id we know it's an
+     * edit. Otherwise we know it's an add.
+     */
+    HeroService.prototype.save = function (hero) {
+        if (hero.id) {
+            return this.put(hero);
+        }
+        return this.post(hero);
+    };
+    // Add a New Hero
+    HeroService.prototype.post = function (hero) {
+        var headers = new http_1.Headers({
+            'Content-Type': 'application/json'
+        });
+        return this.http.post(this.heroesUrl, JSON.stringify(hero), { headers: headers })
+            .toPromise()
+            .then(function (res) { return res.json().data; })
+            .catch(this.handleError);
+    };
+    // Modify an hero
+    HeroService.prototype.put = function (hero) {
+        // Put will be used to update an individual hero. Its structure is very similar to 
+        // Post requests. The only difference is that we have to change the url slightly 
+        // by appending the id of the hero we want to update.
+        var headers = new http_1.Headers();
+        headers.append('Content-Type', 'application/json');
+        var url = this.heroesUrl + "/" + hero.id;
+        return this.http.put(url, JSON.stringify(hero), { headers: headers })
+            .toPromise()
+            .then(function () { return hero; })
+            .catch(this.handleError);
+    };
+    HeroService.prototype.handleError = function (error) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     };
     HeroService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [http_1.Http])
     ], HeroService);
     return HeroService;
 }());
